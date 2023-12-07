ba0db13365c7783c4632185330b54678492a66c1
AnikaSangar <105520850+AnikaSangar@users.noreply.github.com>
2023-12-06 17:47:43 -0700

    Add files via upload

---
+++ employee/main.java
@@ -0,0 +1,11 @@
+package HTML;
+
+public class main{
+
+    public static void main(String[] args) {
+        file employee = new file();
+        //getters
+        //setters
+        //call methods
+    }
+}
